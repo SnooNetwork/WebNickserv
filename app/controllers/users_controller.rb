b8804b84e2e0479de6ec644419d93dd446d18594
nekosune <kat.swales@nekokittygames.com>
2014-05-18 21:26:51 +0100

    Got BASE of login working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
+require "pp"
+require 'xmlrpc/client'
 class UsersController < ApplicationController
+  def login
+    if(not params.nil? and params.key? :login)
+      pp "I have DATA"
+      pp params
+      server=XMLRPC::Client.new2("http://cosmos.snoonet.org:8080/xmlrpc")
+      result=server.call("atheme.login",params[:login][:username],params[:login][:password],request.remote_ip)
+      pp result
+    end
+  end
 end
