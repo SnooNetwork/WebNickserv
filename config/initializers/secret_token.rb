e5af02d14d6481fb245d613a4aa860152cb825ea
nekosune <kat.swales@nekokittygames.com>
2014-05-18 18:49:25 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SnoonetAdmin::Application.config.secret_key_base = 'c9c4be546da0907be08b474d262d345ecf79d57fab4ffa7d86b6387783621aa6f2948856bd9e0d944bb3e978811027d1ed251c78618133315a9d84e11b014c95'
